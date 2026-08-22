2c0a733d39eae4b07ddd112d6ef4282c3fcfb87e
mohamed bekheet <mohamed.bekheet@auranoxai.com>
2026-08-22 04:48:57 +0300

    feat: certifications page reads from supabase with static fallback

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Fade } from "react-awesome-reveal";
-import { certifications } from "./certificationData";
+import { useContent } from "../../hooks/useContent.js";
+
+const toCard = (cert) => ({
+  key: cert.id ?? cert.title,
+  title: cert.title,
+  image: cert.image_url ?? cert.img,
+  alt: cert.alt || cert.title,
+  link: cert.link && cert.link !== "#" ? cert.link : null,
+  description: cert.description
+});
 
 export default function ProjectPage() {
+  const { content } = useContent("certifications");
+  const cards = (content ?? []).map(toCard);
+
   return (
     <section>
       <Container fluid className="project-section">
@@ -20,8 +32,8 @@ export default function ProjectPage() {
                 <Row>
                   <Col md={12} className="mt-5">
                     <Row className="g-5">
-                      {certifications.map((cert, index) => (
-                        <Col key={index} md={3} className="col-sm-12 col-md-4">
+                      {cards.map((cert) => (
+                        <Col key={cert.key} md={3} className="col-sm-12 col-md-4">
                           <Fade bottom>
                             <div className="galaxy-glass-card">
                               <a
@@ -29,12 +41,13 @@ export default function ProjectPage() {
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="certification-link"
+                                style={cert.link ? undefined : { pointerEvents: "none" }}
                               >
                                 <div className="singleProject">
                                   <div className="projectContent">
                                     <h5 className="cert-title">{cert.title}</h5>
                                     <img
-                                      src={cert.img}
+                                      src={cert.image}
                                       alt={cert.alt}
                                       className="cert-image"
                                     />
